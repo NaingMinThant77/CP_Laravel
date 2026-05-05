9983f7c6f49a9097b5b385752bbfd57d0a1ce83d
NaingMinThant77 <naingminthant07@gmail.com>
2026-05-05 10:56:24 +0630

    feat: Category Seeder with Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,30 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Category::create([
-        //     'name' => 'Category 1',
-        //     'description' => 'This is Category 1 description'
-        // ]);
-
-        Category::insert([
-            [
-                'name' => 'Category 2',
-                'description' => 'This is Category 2 description',
-                'created_at' => now(),
-                'updated_at' => now()
-            ],
-            [
-                'name' => 'Category 3',
-                'description' => 'This is Category 3 description',
-                'created_at' => now(),
-                'updated_at' => now()
-            ], 
-            [
-                'name' => 'Category 4',
-                'description' => 'This is Category 4 description',
-                'created_at' => now(),
-                'updated_at' => now()
-            ]
-        ]);
+        Category::factory(10)->create();
     }
 }
