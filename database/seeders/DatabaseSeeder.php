70833f15d682f21f84700c4012dacd60e1742100
NaingMinThant77 <naingminthant07@gmail.com>
2026-05-05 11:18:21 +0630

    feat: Factory With Seeder File

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Category::factory(10)->create();
-
-        Product::factory(10)->create();
+        $this->call([
+            CategorySeeder::class, ProductSeeder::class
+        ]);
     }
 }
